0e5e89569f065f847de94a946424d4955d77bae1
Joseph Demcher <joedemcher@gmail.com>
2018-02-26 14:42:30 -0500

    Add files via upload

---
+++ 006.c
@@ -0,0 +1,37 @@
+/*
+
+Joseph Demcher
+February 2018
+CS 288
+
+A C program that calculates the total size of the files in a directory and the sub-directories of the directory. Similiar to the du command in Linux.
+
+arguments: ./your_program    pathname_of_directory
+
+*/
+
+#define _XOPEN_SOURCE 1
+#define _XOPEN_SOURCE_EXTENDED 1	
+
+#include <stdio.h>
+#include <ftw.h>
+
+static unsigned int total_size = 0;
+
+int sum( const char *fp, const struct stat *st, int flags, struct FTW *ftwb );
+
+int main(int argc, char **argv) {
+	
+	int flag;
+	flag = FTW_PHYS;
+	if ( nftw( argv[1], &sum, flag, 1 ) ) {
+		return -1;
+    	}
+    	printf( "%u\n", total_size );
+    	return 0;
+}
+
+int sum( const char *fp, const struct stat *st, int flags, struct FTW *ftwb ) {
+	total_size += st->st_size;
+	return 0;
+}
